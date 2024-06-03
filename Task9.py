0d7869d954b47294456b3aac3ea3d6ca0f01d53e
Максим Федорко <maximfedorko@gmail.com>
2024-06-03 12:20:36 +0300

    Completed ninth task

---
+++ Task9.py
@@ -0,0 +1,21 @@
+L = [3,6,7]
+my_list = [1818, 29, 123, 2024]
+print(my_list)
+
+my_list.extend(L)
+print(my_list)
+
+my_list[1] = 33333
+print(my_list)
+
+my_list.reverse()
+print(my_list)
+
+my_list.append(3)
+print(my_list)
+
+my_list.remove(3)
+print(my_list)
+
+my_list.sort()
+print(my_list)
