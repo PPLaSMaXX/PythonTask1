703debfd8af4afa772d5997952352bd6303df76a
Максим Федорко <maximfedorko@gmail.com>
2024-06-03 12:14:12 +0300

    Completed eighth task

---
+++ Task8.py
@@ -0,0 +1,5 @@
+allowed_characters = 'abcdefghijklmnopqrstuvwxyz'
+
+m = 11 #password length
+
+print("quantity of possible variations of password of " + str(m) + " length equals to " + str(len(allowed_characters)**m))
