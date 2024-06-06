29c222e30d85039eee3066ea365d9ecf48a4ecd8
Максим Федорко <maximfedorko@gmail.com>
2024-06-06 23:10:17 +0300

    Completed Task2.3

---
+++ Task2/Task3.py
@@ -0,0 +1,11 @@
+to_be = True
+this_will, be_checked = 10, 10
+not_be_checked = 5
+can_i_go_home_faster = False
+x, y = 50, 50
+blue_pill_is_better, red_pill_is_better = False, False
+
+print(to_be or not to_be)
+print(this_will == be_checked or this_will == not_be_checked)
+print(can_i_go_home_faster or x is not y)
+print(blue_pill_is_better or red_pill_is_better)
